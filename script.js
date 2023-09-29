df7511f2e7ee49a02b57c4756d3666c980e27784
MANIMARANFULLSATACKDEVELOPER <rmanimaran911@gmail.com>
2023-09-29 11:20:46 +0530

    24-Aug

---
+++ script.js
@@ -0,0 +1,22 @@
+score = 0;
+cross = true;
+audio = new Audio('./images/music.mp3');
+audiogo = new Audio('./images/gameover.mp3');
+setTimeout(()=>{
+    audio.play();
+},1000);
+document.onkeydown = function(e){
+    console.log("key code is " + e.keyCode);
+    if(e.keyCode ==38){  //up
+        dino = document.querySelector('.dino');
+        dino.classList.add('animateDino');
+        setTimeout(()=>{
+            dino.classList.remove('animateDino');
+        },700);    
+    };
+    if(e.keyCode ==39){  //right
+        dino = document.querySelector('.dino');
+        dinoX = parseInt(window.getComputedStyle(dino,null).getPropertyValue('left'));
+        dino.style.left = dinoX + 112 + "px";
+    }
+}
